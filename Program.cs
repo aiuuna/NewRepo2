f1b719ab237430e2e6f514c451b7de40e4346866
Aiuuna_06610 <bimbazhapovaa@icloud.com>
2023-06-20 17:33:12 +0800

    ready

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<TodoItem> lst = new List<TodoItem>();
+            {
+                lst.Add(new TodoItem(1, "test1", false, "secret1"));
+                lst.Add(new TodoItem(2, "test2", false, "secret2"));
+                lst.Add(new TodoItem(3, "test3", false, "secret3"));
+                lst.Add(new TodoItem(4, "test4", false, "secret4"));
+            }
+            List<TodoItemDTO> lst1 = new List<TodoItemDTO>();{
+                lst1.Add(new TodoItemDTO(1, "test1", false));
+                lst1.Add(new TodoItemDTO(2, "test2", false));
+                lst1.Add(new TodoItemDTO(3, "test3", false));
+                lst1.Add(new TodoItemDTO(4, "test4", false));
+            }
+            var any = from p in lst
+                      from c in lst1
+                      where c.id == p.id1
+                      where c.name == p.name1
+                        select new
+                        {
+                            id = c.id,
+                            name = c.name
+                        };
+            foreach (var c in any)
+            {
+                Console.WriteLine($" {c.id,35}\t {c.name,10}");
+            }
+        }
+
+    }
+}
---
+++ ToDoItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class TodoItemDTO
+    {
+        public int id;
+        public string? name;
+        public bool isComplete;
+
+        public TodoItemDTO(int Id, string? Name,bool IsComplete )
+        {
+            id = Id;
+            name=Name;
+        }
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+    class TodoItem
+    {
+
+        public int id1;
+        public string? name1;
+        public bool isComplete1;
+        public TodoItem(int Id, string? Name, bool IsComplete,  string? Secret)
+        {
+            id1=Id;
+            name1 = Name;
+        }
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string? Secret { get; set; }
+    }
+
+}
